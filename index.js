a757861bc65cb827a4c7a42e024da4143ee266f4
Crewmasta <41262507+Crewmasta@users.noreply.github.com>
2018-08-09 22:46:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
 
+bot.on("ready", async () => {
+ console.log (`${bot.user.username} is online`);
+  bot.user.setGame("Empire.");
+});
+var prefix = ".";
+if(cmd ==== `${prefix}botinfo`){
+
+ let empire = new Discord.RichEmbed()
+ .setDescription("Bot information.")
+ .setColor("RANDOM")
+ .addField("Bot name", bot.user.username);
+  
+  return message.channe.send(empire);
+ 
+}
+
+
+client.login(process.env.BOT_TOKEN); 

c7ca1697027bb3b2778ad246a359000cf97f6d79
Crewmasta <41262507+Crewmasta@users.noreply.github.com>
2018-08-09 23:02:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,19 @@ empire.on("ready", async () => {
  console.log (`${empire.user.username} is online`);
   empire.user.setGame("Empire.");
 });
-var prefix = ".";
-client.on("mesage", cmd => {
-if("cmd.content === "${prefix}botinfo");
 
- let empire = new Discord.RichEmbed()
- .setDescription("Bot information.")
- .setColor("RANDOM")
- .addField("Bot name", bot.user.username);
-  
-  return message.channe.send(empire);
+empire.on("message", empire => {
+if(empire.content === ".botinfo";
+const empire = new Discord.RichEmbed()
+ .setTitle("~~__***Bot info***__~~.")
+ .setAuthor("Idk", "https://i.imgur.com/lm8s41J.png")
  
-}
-}
+ .setColor(RANDOM)
+ .setDescription("Just anormal bot info tho")
+ .addField("Bot name", "${empire.user.username}, true)
+   
+   }
+   message.channel.send({empire});
+
 
 client.login(process.env.BOT_TOKEN); 
